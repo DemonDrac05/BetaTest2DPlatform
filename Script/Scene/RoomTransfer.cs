6ac2ad69a91e285a231aa7f0fce1d437adb39e8c
Tien Thang Phan <149472651+DemonDrac05@users.noreply.github.com>
2024-02-23 00:13:51 +0700

    Fix Room Transfer + Add Transfer Animation

--- Script/Scene/RoomTransfer.cs
+++ Script/Scene/RoomTransfer.cs
@@ -12,21 +12,55 @@ public class RoomTransfer : MonoBehaviour
 
     [field: SerializeField] public Vector3 playerChangePos;
 
+    public Player player;
+
     public CameraMovement cam;
+
+    public float orgCamMinPos, orgCamMaxPos;
+
+    public RoomTransitionAnimation transition;
+
+    public bool transferred;
 
     private void Start()
     {
         PlayerTarget = GameObject.FindGameObjectWithTag("Player");
 
+        player = FindObjectOfType<Player>();
+
         cam = FindObjectOfType<CameraMovement>();
+
+        transition = FindObjectOfType<RoomTransitionAnimation>();
+
+        orgCamMinPos = cam.minPosition.x;
+        orgCamMaxPos = cam.maxPosition.x;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject == PlayerTarget)
         {
-            cam.minPosition.x = cam.maxPosition.x + addMinBorder;
-            cam.maxPosition.x = cam.minPosition.x + addMaxBorder;
-            collision.transform.position += playerChangePos;
+            transferred = true;
+
+            if (player.IsFacingRight)
+            {
+                cam.minPosition.x = cam.maxPosition.x + addMinBorder;
+                cam.maxPosition.x = cam.minPosition.x + addMaxBorder;
+                collision.transform.position += playerChangePos;
+            }
+
+            else if (!player.IsFacingRight)
+            {
+                cam.minPosition.x = orgCamMinPos;
+                cam.maxPosition.x = orgCamMaxPos;
+                collision.transform.position -= playerChangePos;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == PlayerTarget)
+        {
+            transferred = false;
         }
     }
 }
